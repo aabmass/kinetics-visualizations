37675ef34d24d15a4d6456bb2b39a43cba4f8859
Aaron Abbott <aabmass@gmail.com>
2016-11-02 01:34:48 -0400

    Working with babel loader and webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+module.exports = {
+  entry: './js/app.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
